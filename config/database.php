5a35fb2c573b825b1d58d2611aaee6af2103c0a8
Oluwakayode Adesina <adesinak4@gmail.com>
2022-01-10 18:49:24 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -47,34 +47,14 @@ return [
         ],
         
 
-//         'mysql' => [
-//             'driver' => 'mysql',
-//             'url' => env('DATABASE_URL'),
-//             'host' => env('DB_HOST', '127.0.0.1'),
-//             'port' => env('DB_PORT', '3306'),
-//             'database' => env('DB_DATABASE', 'forge'),
-//             'username' => env('DB_USERNAME', 'forge'),
-//             'password' => env('DB_PASSWORD', ''),
-//             'unix_socket' => env('DB_SOCKET', ''),
-//             'charset' => 'utf8mb4',
-//             'collation' => 'utf8mb4_unicode_ci',
-//             'prefix' => '',
-//             'prefix_indexes' => true,
-//             'strict' => true,
-//             'engine' => null,
-//             'options' => extension_loaded('pdo_mysql') ? array_filter([
-//                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-//             ]) : [],
-//         ],
-        
-        'your_heroku_mysql_connection' => [
+        'mysql' => [
             'driver' => 'mysql',
-            'url' => $url = parse_url(getenv("CLEARDB_DATABASE_URL"));,
-            'host' => env('DB_HOST', 'us-cdbr-east-05.cleardb.net'),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'heroku_6e3cb1382e819f4'),
-            'username' => env('DB_USERNAME', 'bc9968244cb506'),
-            'password' => env('DB_PASSWORD', '0965c09e'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -86,6 +66,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        
 
         'pgsql' => [
             'driver' => 'pgsql',
