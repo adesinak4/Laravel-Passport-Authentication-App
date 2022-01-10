271e1fd31f9b17a0c6ba635b2ef925d672854061
Oluwakayode Adesina <adesinak4@gmail.com>
2022-01-10 18:40:59 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -34,6 +34,8 @@ return [
     */
 
     'connections' => [
+        
+        
 
         'sqlite' => [
             'driver' => 'sqlite',
@@ -44,14 +46,34 @@ return [
         ],
         
 
+//         'mysql' => [
+//             'driver' => 'mysql',
+//             'url' => env('DATABASE_URL'),
+//             'host' => env('DB_HOST', '127.0.0.1'),
+//             'port' => env('DB_PORT', '3306'),
+//             'database' => env('DB_DATABASE', 'forge'),
+//             'username' => env('DB_USERNAME', 'forge'),
+//             'password' => env('DB_PASSWORD', ''),
+//             'unix_socket' => env('DB_SOCKET', ''),
+//             'charset' => 'utf8mb4',
+//             'collation' => 'utf8mb4_unicode_ci',
+//             'prefix' => '',
+//             'prefix_indexes' => true,
+//             'strict' => true,
+//             'engine' => null,
+//             'options' => extension_loaded('pdo_mysql') ? array_filter([
+//                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+//             ]) : [],
+//         ],
+        
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => $url = parse_url(getenv("CLEARDB_DATABASE_URL"));,
+            'host' => env('DB_HOST', 'us-cdbr-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_6e3cb1382e819f4'),
+            'username' => env('DB_USERNAME', 'bc9968244cb506'),
+            'password' => env('DB_PASSWORD', '0965c09e'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
